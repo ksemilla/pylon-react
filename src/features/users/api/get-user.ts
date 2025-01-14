9ba2ea2391dbe91c3e89419bd1876c835c53922a
ksemilla <kevinsemilla@gmail.com>
2025-01-14 23:46:48 +1100

    Replace get user api

---
+++ src/features/users/api/get-user.ts
@@ -0,0 +1,27 @@
+import { api } from "@/lib/api-client"
+import { QueryConfig } from "@/lib/react-query"
+import { User } from "@/types/users"
+import { queryOptions, useQuery } from "@tanstack/react-query"
+
+export const getUser = (id: number) => {
+  return api.get<User>(`users/${id}/`)
+}
+
+export const getUserQueryOptions = (id: number) => {
+  return queryOptions({
+    queryKey: ["user", id],
+    queryFn: () => getUser(id),
+  })
+}
+
+type UseUserOptions = {
+  id: number
+  queryConfig?: QueryConfig<typeof getUserQueryOptions>
+}
+
+export const useDiscussion = ({ id, queryConfig }: UseUserOptions) => {
+  return useQuery({
+    ...getUserQueryOptions(id),
+    ...queryConfig,
+  })
+}
