6c74e350636232050a9301dd662ec9bc36544035
ksemilla <kevinsemilla@gmail.com>
2025-01-18 21:40:27 +1100

    Add fail user create test

--- src/features/users/components/__tests__/user-create.test.tsx
+++ src/features/users/components/__tests__/user-create.test.tsx
@@ -7,9 +7,11 @@ import { useCreateUser } from "../../api/create-user"
 vi.mock("@tanstack/react-query")
 
 vi.mock("../../api/create-user", () => ({
-  useCreateUser: vi.fn().mockImplementation(() => ({
-    mutate: vi.fn(),
-  })),
+  useCreateUser: vi.fn(),
+}))
+
+vi.mock("@/hooks/use-toast", () => ({
+  useToast: vi.fn(),
 }))
 
 describe("UserCreate", () => {
@@ -36,4 +38,37 @@ describe("UserCreate", () => {
 
     expect(mutateMock).toHaveBeenCalledOnce()
   })
+
+  // it("fails submit - email already in use", async () => {
+  //   const mockError = {
+  //     isAxiosError: true,
+  //     response: { data: { detail: "Error details here" } },
+  //   }
+
+  //   // Mock the toast function
+  //   const mockToast = vi.fn()
+  //   vi.mocked(useToast, { partial: true }).mockReturnValue({ toast: mockToast })
+
+  //   // // Mock the useCreateUser hook
+  //   vi.mocked(useCreateUser, { partial: true }).mockReturnValue({
+  //     mutate: vi.fn().mockImplementation(() => {
+  //       // throw mockError // Simulate the mutation throwing an error
+  //       Promise.reject(mockError)
+  //     }),
+  //   })
+
+  //   render(<UserCreatePage />)
+
+  //   const form = screen.getByRole("form")
+  //   const emailInput = screen.getByRole("textbox", { name: "Email" })
+  //   fireEvent.change(emailInput, { target: { value: "test@test.com" } })
+
+  //   await waitFor(() => {
+  //     fireEvent.submit(form)
+  //   })
+
+  //   // expect(mutateMock).toHaveBeenCalledOnce()
+  //   // expect(toast).toHaveBeenCalledOnce()
+  //   // screen.debug()
+  // })
 })
