318da4f8bd869e3e100ed4c5aea1e3c0af9ac503
ksemilla <kevinsemilla@gmail.com>
2025-01-20 22:38:22 +1100

    Mock api call for failed submit user create

--- src/features/users/components/__tests__/user-create.test.tsx
+++ src/features/users/components/__tests__/user-create.test.tsx
@@ -3,49 +3,65 @@ import { it, vi, expect } from "vitest"
 import { describe } from "vitest"
 import { UserCreatePage } from "../user-create"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { createUser } from "../../api/create-user"
-// import * as TRQ from "@tanstack/react-query"
+import * as Toast from "@/hooks/use-toast"
+import * as UCU from "../../api/create-user"
 
-// vi.mock("@tanstack/react-query", async () => {
-//   const actual = await vi.importActual<typeof TRQ>("@tanstack/react-query")
-//   return {
-//     ...actual,
-//     // useMutation: () => ({
-//     //   mutate: vi.fn().mockImplementation(() => {
-//     //     throw new Error("lol")
-//     //   }),
-//     // }),
-//     useQueryClient: vi.fn(),
-//   }
-// })
+const queryClient = new QueryClient()
 
-vi.mock("../../api/create-user.ts", { spy: true })
+vi.mock("@/lib/api-client", () => ({
+  api: {
+    post: vi.fn().mockRejectedValue(new Error("lol")),
+  },
+}))
 
-// vi.mock("../../api/create-user", async (importOriginal) => {
-//   const actual: object = await importOriginal()
-//   return {
-//     ...actual,
-//     createUser: vi.fn(),
-//   }
-// })
-
-// vi.mock("@/hooks/use-toast", () => ({
-//   useToast: vi.fn(),
-// }))
+vi.mock("@/lib/utils", () => ({
+  log: vi.fn(),
+  cn: vi.fn(),
+}))
 
 describe("UserCreate", () => {
-  // it("renders user create page", async () => {
-  //   const mutateMock = vi.fn()
-  //   ;(useCreateUser as Mock).mockReturnValue({ mutate: mutateMock })
-  //   render(<UserCreatePage />)
+  it("renders user create page", async () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <UserCreatePage />
+      </QueryClientProvider>
+    )
 
-  //   expect(screen.getByRole("form")).toBeInTheDocument()
-  // })
+    expect(screen.getByRole("form")).toBeInTheDocument()
+  })
 
   // it("success submit", async () => {
   //   const mutateMock = vi.fn()
-  //   ;(useCreateUser as Mock).mockReturnValue({ mutate: mutateMock })
-  //   render(<UserCreatePage />)
+  //   const useCreateUserMock = vi.spyOn(UCU, "useCreateUser")
+  //   useCreateUserMock.mockReturnValue({
+  //     data: {
+  //       data: { email: "", role: "user", isActive: true },
+  //       status: 200,
+  //       statusText: "",
+  //       headers: {},
+  //       // config: { headers: { set: () => {} } },
+  //     },
+  //     error: { message: "", name: "" },
+  //     isError: false,
+  //     isIdle: false,
+  //     isPending: false,
+  //     isPaused: false,
+  //     isSuccess: false,
+  //     failureCount: 0,
+  //     failureReason: { message: "", name: "" },
+  //     mutate: mutateMock,
+  //     mutateAsync: vi.fn(),
+  //     reset: vi.fn(),
+  //     status: "success",
+  //     submittedAt: 0,
+  //     variables: { email: "", role: "user", isActive: true },
+  //   })
+
+  //   render(
+  //     <QueryClientProvider client={queryClient}>
+  //       <UserCreatePage />
+  //     </QueryClientProvider>
+  //   )
 
   //   const form = screen.getByRole("form")
   //   const emailInput = screen.getByRole("textbox", { name: "Email" })
@@ -55,32 +71,18 @@ describe("UserCreate", () => {
   //     fireEvent.submit(form)
   //   })
 
-  //   expect(mutateMock).toHaveBeenCalledOnce()
+  //   expect(useCreateUserMock).toHaveBeenCalledTimes(1)
   // })
 
   it("fails submit - email already in use", async () => {
-    // const mockError = {
-    //   isAxiosError: true,
-    //   response: { data: { detail: "Error details here" } },
-    // }
+    const toast = vi.fn()
+    const useToastSpy = vi.spyOn(Toast, "useToast")
+    useToastSpy.mockReturnValue({
+      toast,
+      dismiss: vi.fn(),
+      toasts: [],
+    })
 
-    // // Mock the toast function
-    // const mockToast = vi.fn()
-    // vi.mocked(useToast, { partial: true }).mockReturnValue({ toast: mockToast })
-
-    // // Mock the useCreateUser hook
-    // vi.mocked(useCreateUser, { partial: true }).mockReturnValue({
-    //   mutate: vi.fn().mockImplementation(() => {
-    //     // throw mockError // Simulate the mutation throwing an error
-    //     Promise.reject(mockError)
-    //   }),
-    // })
-
-    // vi.mocked(createUser).mockRejectedValue({
-    //   isAxiorError: true,
-    // })
-
-    const queryClient = new QueryClient()
     render(
       <QueryClientProvider client={queryClient}>
         <UserCreatePage />
@@ -95,8 +97,6 @@ describe("UserCreate", () => {
       fireEvent.submit(form)
     })
 
-    // expect(mutateMock).toHaveBeenCalledOnce()
-    expect(createUser).toHaveBeenCalledOnce()
-    // screen.debug()
+    expect(toast).toHaveBeenCalledOnce()
   })
 })
