03ac2ad0229ebb86a66d8735c4eb164626a5dfae
ksemilla <kevinsemilla@gmail.com>
2025-01-24 16:05:25 +1100

    Add create member

--- src/features/members/components/member-create.tsx
+++ src/features/members/components/member-create.tsx
@@ -1,10 +1,16 @@
 import { HasMemberPermission } from "@/components/custom/has-member-permission"
 import { MemberPermissionEnum } from "../permissions"
+import { MemberCreateForm } from "./member-create-form"
+import { useCreateMember } from "../api/create-member"
 
 export function MemberCreatePage() {
+  const { mutate } = useCreateMember()
+
   return (
     <HasMemberPermission permission={MemberPermissionEnum.MEMBERS_CREATE}>
-      <div>Member create</div>
+      <div className="max-w-xl">
+        <MemberCreateForm onSubmit={mutate} />
+      </div>
     </HasMemberPermission>
   )
 }
